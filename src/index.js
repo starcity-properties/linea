4599403bb276272a9097a98e2cc1ffda1ec52943
Queenie Ho <kuini.ho@gmail.com>
2017-11-03 10:56:01 -0700

    render room outline

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import Snap from 'snapsvg';
+import registerServiceWorker from './registerServiceWorker';
+import Snap from 'snapsvg-cjs';
+// const Snap = window.Snap;
+
+// import Snap from 'snapsvg';
 
 // console.log("TEST");
 
 // console.log("room information: ");
 
-var room = {
+var roomData = {
     unit: {
         name: "room-01",
         code: "mission-room-01",
@@ -23,34 +27,38 @@ var room = {
             { x: 10, y: 280, radius: 0, curve: "none", index: 7 },
         ]
     },
-    features: [
-        {
-            type: "window",
-            label: "window01-room-01",
-            outline: [
-                { x: 10, y: 30, radius: 0, curve: "none", index: 0 },
-                { x: 10, y: 100, radius: 0, curve: "none", index: 1 }
-            ]
-        },
-        {
-            type: "window",
-            label: "window02-room-01",
-            outline: [
-                { x: 10, y: 150, radius: 0, curve: "none", index: 0 },
-                { x: 10, y: 260, radius: 0, curve: "none", index: 1 }
-            ]
-        },
-        {
-            type: "door",
-            label: "door-room-01",
-            outline: [
-                { x: 380, y: 230, radius: 0, curve: "none", index: 0 },
-                { x: 380, y: 270, radius: 0, curve: "none", index: 1 },
-                { x: 340, y: 230, radius: 40, curve: "none", index: 2 }
-                // we might need to math the radius for the door by calculating the distance between p1 and p2
-            ]
-        }
-    ]
+    features: {
+        windows: [
+            {
+                type: "window",
+                label: "window01-room-01",
+                outline: [
+                    { x: 10, y: 30, radius: 0, curve: "none", index: 0 },
+                    { x: 10, y: 100, radius: 0, curve: "none", index: 1 }
+                ]
+            },
+            {
+                type: "window",
+                label: "window02-room-01",
+                outline: [
+                    { x: 10, y: 150, radius: 0, curve: "none", index: 0 },
+                    { x: 10, y: 260, radius: 0, curve: "none", index: 1 }
+                ]
+            }
+        ],
+        door: [
+            {
+                type: "door",
+                label: "door-room-01",
+                outline: [
+                    { x: 380, y: 230, radius: 0, curve: "none", index: 0 },
+                    { x: 380, y: 270, radius: 0, curve: "none", index: 1 },
+                    { x: 340, y: 230, radius: 40, curve: "none", index: 2 }
+                    // we might need to math the radius for the door by calculating the distance between p1 and p2
+                ]
+            }
+        ]
+    }
 }
 
 
@@ -70,22 +78,80 @@ class Floorplan {
     constructor(id, minX, minY, x, y) {
         // Create Snap with ID associated with HTML SVG. e.g. <svg id="svg">
         this.paper = Snap(id);
+
         // Creates a responsive viewBox of dimensions x and y, with optional minX and minY
+        // Separate function for making the string to pass into viewBox?
         this.paper.attr({ viewBox: minX + " " + minY + " " + x + " " + y });
-        this.paper.attr({ border: "solid 1px lightgrey" });
-        this.paper.attr({ padding: "10px" });
-       // this.circle = this.paper.circle(90,120,80);
+
+        // Initialize walls
+        this.wall = [];
+
+        // Initialize windows
+        this.windows = [];
+
+        // Initialize door
+        this.doors = [];
+
+        // Initialize other features??
+
     }
 
-    drawCircle(x, y, z) {
-        console.log(x + " " + y + " "+ z);
-        this.paper.circle(x, y, z).attr({
-            fill: "black"
+    // Function to create the string to draw path.
+    compilePath(points) {
+        var path = [];
+
+        // Iterates through each point object to create path
+        // M starts a path
+        // A indicates an arc. Uses: xRadius, yRadius, rotation, arc-flag, sweep-flag, endX, endY.
+        // L indicate a straight line to (x, y)
+        // Z closes the shape of the path
+        points.forEach((point, index) => {
+            if (index == 0)
+                path.push("M", point.x, point.y);
+            else if (point.radius > 0 && point.curve === "concave")
+                path.push("A", point.radius, point.radius, 0, 0, 1, point.x, point.y);
+            else if (point.radius > 0 && point.curve === "convex")
+                path.push("A", point.radius, point.radius, 0, 0, 0, point.x, point.y);
+            else
+                path.push("L", point.x, point.y);
+        })
+        path.push("Z");
+
+        // make a string out of path
+        return (path.join(" "));
+    }
+
+    drawRoomOutline(points, fill, strokeColor, strokeWidth) {
+        // compilePath() takes points and makes the string to pass into path? 
+        this.wall = this.paper.path(this.compilePath(points));
+
+        // give style to shape
+        this.wall.attr({
+            fill: fill,
+            stroke: strokeColor,
+            strokeWidth: strokeWidth
+        })
+    }
+
+    drawWindows(windows, strokeColor, strokeWidth) {
+        windows.forEach((window) => {
+            console.log(window);
+            drawLine(window, strokeColor, strokeWidth);
         });
     }
+
+    // drawCircle(x, y, z) {
+    //     console.log(x + " " + y + " "+ z);
+    //     this.paper.circle(x, y, z).attr({
+    //         stroke: "black",
+    //         strokeWidth: 3,
+    //         fill: "black"
+    //     });
+    // }
 }
 
 
-var newData = new Floorplan("#svg", 1000, 800, 600, 400);
-//var newData = new Floorplan("#svg", windowWidth, windowHeight, 600, 400);
-newData.drawCircle(90, 120, 80);
+var roomRender = new Floorplan("#svg", 0, 0, windowWidth, windowHeight);
+// roomRender.drawCircle(90, 120, 80);
+roomRender.drawRoomOutline(roomData.room.outline, "lightgray", "black", 5);
+roomRender.drawWindows(roomData.features.windows, "cyan", 3);
