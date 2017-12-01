9b4a843fb633c8dafd731e8dd73996dbe998faa5
Andres Pineda <humb.amp123@gmail.com>
2017-12-01 15:17:58 -0800

    refactor code and draw floors

--- src/testRoom.js
+++ src/testRoom.js
@@ -6,20 +6,53 @@ export var testRoom = {
         number: 1,
         scale: 1
     },
-    room: {
-        outline: [
-            { x: 10, y: 10, radius: 0, curve: "none", index: 0 },
-            { x: 210, y: 10, radius: 0, curve: "none", index: 1 },
-            { x: 280, y: 100, radius: 100, curve: "concave", index: 2 },
-            { x: 280, y: 180, radius: 0, curve: "none", index: 3 },
-            { x: 310, y: 220, radius: 50, curve: "convex", index: 4 },
-            { x: 380, y: 220, radius: 0, curve: "none", index: 5 },
-            { x: 380, y: 280, radius: 0, curve: "none", index: 6 },
-            { x: 10, y: 280, radius: 0, curve: "none", index: 7 },
-        ]
+    floor: {
+      unit: {
+        origin: { x: 10, y: 10 },
+        code: "testFloor"
+      },
+      outline: [
+        { x: 0, y: 0, radius: 0, curve: "none", index: 0 },
+        { x: 590, y: 0, radius: 0, curve: "none", index: 1 },
+        { x: 590, y: 590, radius: 0, curve: "none", index: 2 },
+        { x: 0, y: 590, radius: 0, curve: "none", index: 3 }
+      ]
     },
-    features: {
-        windows: [
+    rooms:[
+        {
+            unit: {
+                name: "room-01",
+                code: "mission-room-01",
+                rate: 2100,
+                number: 1,
+                scale: 1,
+                origin: {x: 0, y: 0}
+            },
+            outline: [
+                { x: 10, y: 10, radius: 0, curve: "none", index: 0 },
+                { x: 210, y: 10, radius: 0, curve: "none", index: 1 },
+                { x: 280, y: 100, radius: 100, curve: "concave", index: 2 },
+                { x: 280, y: 180, radius: 0, curve: "none", index: 3 },
+                { x: 310, y: 220, radius: 50, curve: "convex", index: 4 },
+                { x: 380, y: 220, radius: 0, curve: "none", index: 5 },
+                { x: 380, y: 280, radius: 0, curve: "none", index: 6 },
+                { x: 10, y: 280, radius: 0, curve: "none", index: 7 },
+            ]
+        },
+        {
+          unit: {
+            code: "testing",
+            origin: { x: 10, y: 130 }
+          },
+          outline: [
+            { x: 0, y: 0, radius: 0, curve: "none", index: 0 },
+            { x: 0, y: 150, radius: 0, curve: "none", index: 1 },
+            { x: 210, y: 150, radius: 0, curve: "none", index: 2 },
+            { x: 210, y: 0, radius: 0, curve: "none", index: 3 }
+          ]
+        }
+    ],
+    features: [
             {
                 type: "window",
                 code: "room-01-window01",
@@ -37,9 +70,7 @@ export var testRoom = {
                     { x: 10, y: 150, radius: 0, curve: "none", index: 0 },
                     { x: 10, y: 260, radius: 0, curve: "none", index: 1 }
                 ]
-            }
-        ],
-        doors: [
+            },
             {
                 type: "door",
                 code: "room-01-door01",
@@ -79,9 +110,7 @@ export var testRoom = {
                     { x: 80, y: 130, index: 0 },
                     { x: 40, y: 130, index: 1 },
                 ]
-            }
-        ],
-        slidingDoors: [
+            },
             {
                 type: "slidingDoor",
                 code: "room-01-slidingDoor01",
@@ -91,9 +120,7 @@ export var testRoom = {
                     { x: 220, y: 180, index: 1},
                     { x: 220, y: 130, index: 2}
                 ]
-            }
-        ],
-        interiorWalls: [
+            },
             {
                 type: "interiorWall",
                 code: "room-01-interiorWall01",
@@ -120,48 +147,39 @@ export var testRoom = {
                     { x: 110, y: 10, index: 0 },
                     { x: 110, y: 90, index: 1 }
                 ]
+            },
+            {
+              type: "queen",
+              code: "room-01-bed01",
+              label: "Queen Bed",
+              outline: [
+                { x: 30, y: 270, index: 0 },
+                { x: 30, y: 210, index: 1 },
+                { x: 110, y: 210, index: 2 },
+                { x: 110, y: 270, index: 3 }
+              ]
+            },
+            {
+                type: "dresser",
+                code: "room-01-dresser01",
+                label: "Dresser",
+                outline: [
+                  { x: 90, y: 170, index: 0 },
+                  { x: 90, y: 140, index: 1 },
+                  { x: 180, y: 140, index: 2 },
+                  { x: 180, y: 170, index: 3 }
+                ]
+            },
+            {
+              type: "nightTable",
+                code: "room-01-nightTable01",
+                  label: "NightTable",
+                    outline: [
+                      { x: 30, y: 175, index: 0 },
+                      { x: 30, y: 205, index: 0 },
+                      { x: 60, y: 205, index: 0 },
+                      { x: 60, y: 175, index: 1 }
+                    ]
             }
-        ],
-        furniture: {
-            beds:[
-                {
-                    type: "queen",
-                    code: "room-01-bed01",
-                    label: "Queen Bed",
-                    outline: [
-                        { x: 30, y: 270, index: 0 },
-                        { x: 30, y: 210, index: 1 },
-                        { x: 110, y: 210, index: 2 },
-                        { x: 110, y: 270, index: 3 }
-                    ]
-                }
-            ],
-            dressers: [
-                {
-                    type: "dresser",
-                    code: "room-01-dresser01",
-                    label: "Dresser",
-                    outline: [
-                        { x: 90, y: 170, index: 0 },
-                        { x: 90, y: 140, index: 1 },
-                        { x: 180, y: 140, index: 2 },
-                        { x: 180, y: 170, index: 3 }
-                    ]
-                }
-            ],
-            nightTables: [
-                {
-                    type: "nightTable",
-                    code: "room-01-nightTable01",
-                    label: "NightTable",
-                    outline: [
-                        { x: 30, y: 175, index: 0 },
-                        { x: 30, y: 205, index: 0 },
-                        { x: 60, y: 205, index: 0 },
-                        { x: 60, y: 175, index: 1 }
-                    ]
-                }
-            ]
-        }
-    }
+        ]
 };
